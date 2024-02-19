effd981229054464dbab253a0b7626a0a20c9546
Tayyaba Amjad Rao <160313142+TayyabaAmjadRao@users.noreply.github.com>
2024-02-19 12:31:09 -0800

    Add files via upload

---
+++ Stripping_Name.js
@@ -0,0 +1,5 @@
+"use strict";
+const personName = '   \n\tTayyaba Rao\n\t   ';
+console.log(personName);
+const without_Whitespace = personName.trim();
+console.log(without_Whitespace);
---
+++ Stripping_Name.ts
@@ -0,0 +1,8 @@
+const personName :string= '   \n\tTayyaba Rao\n\t   ';
+
+
+console.log(personName);
+
+const without_Whitespace :string = personName.trim();
+
+console.log(without_Whitespace);
